82dd919905540c2450d8325565165f3930f25b63
Luccas <luccas.carvalho@gmail.com>
2019-10-16 10:57:53 -0300

    bug corrigido

--- lib/GPS.dart
+++ lib/GPS.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:gps/gps.dart';
+import 'package:geolocator/geolocator.dart';
+
 
 class GPS extends StatefulWidget {
   @override
   _GPSState createState() => _GPSState();
 }
 
-String _latitude;
-String _longitude;
-
-
-void main () async {
-  var latlng = await Gps.currentGps();
-  
-  _latitude = latlng.lat;
-  _longitude = latlng.lng;
-}
-
-
-
 class _GPSState extends State<GPS> {
+  Geolocator geolocator = Geolocator();
 
+  Position userLocation;
+  void initState() {
+    super.initState();
+    _getLocation().then((position) {
+      userLocation = position;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,16 +27,52 @@ class _GPSState extends State<GPS> {
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch, 
-          children: <Widget>[
-              Text(
-              "Sua posição atual está na latitude"+ " " + "$_latitude"+ " " + "e na longitude" + " " + "$_longitude",
-              style: TextStyle(fontSize: 26, color: Colors.white),
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              userLocation == null
+                  ? CircularProgressIndicator()
+                  : Text(
+                      "Sua posição atual está na latitude" +
+                          " " +
+                          userLocation.latitude.toString() +
+                          " " +
+                          "e na longitude" +
+                          " " +
+                          userLocation.longitude.toString(),
+                      style: TextStyle(fontSize: 26, color: Colors.white),
+                    ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    _getLocation().then((value) {
+                      setState(() {
+                        userLocation = value;
+                      });
+                    });
+                  },
+                  color: Colors.lightGreenAccent[300],
+                  child: Text(
+                    "Buscar Localização",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
               ),
-          ],
+            ],
           ),
         ),
-        ),
+      ),
     );
+  }
+
+  Future<Position> _getLocation() async {
+    var currentLocation;
+    try {
+      currentLocation = await geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best);
+    } catch (e) {
+      currentLocation = null;
+    }
+    return currentLocation;
   }
 }
